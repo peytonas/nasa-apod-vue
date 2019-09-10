52a06fc995d1535bc3114a7187c143752b1c08f2
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-09-10 14:10:30 -0600

    logo and colorized

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    NASA
+    <img alt="Vue logo" src="../assets/nasa-logo-web-rgb.png" style="height: 200px;" />APOD
+    <HelloWorld msg="NASA's Astronomy Photo Of the Day" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
